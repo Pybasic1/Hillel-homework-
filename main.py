fa433283941c96617408d8c743a374729b1283a2
Pybasic1 <137644325+Pybasic1@users.noreply.github.com>
2023-08-28 15:48:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
-'''
-import this(это пасхалка с шивром в которой заложена шутка)
-import antigravity(одна из ссылок на пасхалку,конкретно эта на комикс)
-import __hello__(обычный Hello World)
-from __future__ import braces(также пасхалка в которой вместо пропусков должны быть скобки)
-'''
-print (250 + 100)
-print (9 / 3)
-print (200 - 140)
-print (2 ** 3)
-print ('hillel\nbasic')
-'''
-escape последовательности я прочел и понял но что написать в домашку не понял
-'''
+'''Lesson6'''
+
+'''task1'''
+def merge_tuples_to_dict(keys, values):
+    result_dict = {keys[i]: values[i] for i in range(len(keys))}
+    return result_dict
+
+coin = ('Bitcoin', 'Ether', 'Ripple', 'Litecoin')
+code = ('BTC', 'ETH', 'XRP', 'LTC')
+
+merged_dict = merge_tuples_to_dict(coin, code)
+print(merged_dict)
+
+'''task2-не смог разобраться(('''
+
+'''task3'''
+def list_to_dict(lst):
+    result_dict = {index: value for index, value in enumerate(lst)}
+    return result_dict
+
+input_list = ['a', 'b', 'c', 'd', 'e']
+
+output_dict = list_to_dict(input_list)
+print(output_dict)
+
+'''task4'''
+import time
+
+def countdown_decorator(func):
+    def wrapper():
+        for i in range(3, 0, -1):
+            print(i)
+            time.sleep(1)
+        return func()
+    return wrapper
+
+@countdown_decorator
+def what_time_is_it_now():
+    current_time = time.strftime('%H:%M')
+    return current_time
+
+result = what_time_is_it_now()
+print(result)
